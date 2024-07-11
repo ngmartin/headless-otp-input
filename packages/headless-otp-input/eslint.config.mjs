c9eb26bbb2520d0830e1f32eb29933b0b9fd80a0
ngmartin <martingk111@gmail.com>
2024-07-11 23:58:59 +0700

    feat: config lint

---
+++ packages/headless-otp-input/eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { fixupPluginRules } from "@eslint/compat";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["dist/*"] },
+  {
+    languageOptions: {
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser },
+    },
+  },
+  {
+    plugins: {
+      react: eslintPluginReact,
+      "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+    },
+  },
+  {
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier,
+];
